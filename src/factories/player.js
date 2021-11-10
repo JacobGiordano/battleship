632276df095b2391284b72bb82c3eb5773ffab84
JacobGiordano <jacob.giordano12@gmail.com>
2021-11-10 06:38:01 -0800

    Add getIndex, add timeout to comp turn, move cols & rows

--- src/factories/player.js
+++ src/factories/player.js
@@ -1,5 +1,7 @@
-const Player = (name, isComputerBool) => {
+const Player = (name, isComputerBool, isPlayerTurn) => {
   let shotsFired = [];
+  const rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+  const columns = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
   const getName = () => {
     return name;
@@ -28,32 +30,41 @@ const Player = (name, isComputerBool) => {
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
   }
 
-  const getRandCoord = () => {
-    const rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-    const columns = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  const getRandCoords = () => {
     const letter = rows[getRandInclusive(0, rows.length -1)];
     const number = columns[getRandInclusive(0, columns.length -1)];
     return `${letter}${number}`;
   }
 
-  const computerTurn = (forcedCoord) => {
-    let coord = "";
+  const getIndex = coords => {
+    const letter = coords.slice(0, 1);
+    const numbers = coords.slice(1, coords.length);
+    return (rows.indexOf(letter) * 10) + columns.indexOf(numbers);
+  }
 
-    if (forcedCoord !== undefined) {
-      coord = forcedCoord;
-      recordShotFired(coord);
+  const computerTurn = (forcedCoords) => {
+    let coords = "";
+
+    if (forcedCoords !== undefined) {
+      coords = forcedCoords;
+      recordShotFired(coords);
     } else {
-      coord = getRandCoord();
+      coords = getRandCoords();
     }
 
-    if (getShotsFired().indexOf(coord) > -1) {
+    if (getShotsFired().indexOf(coords) > -1) {
       computerTurn();
     } else {
-      recordShotFired(coord);
+      setTimeout(() => {
+        recordShotFired(coords);
+        const squareIndex = getIndex(coords);
+        const square = Array.from(document.querySelectorAll(".board-square"))[squareIndex];
+        square.click();
+      }, 500);
     }
   }
 
-  return {getName, isComputer, attackOpponent, recordShotFired, getShotsFired, getRandCoord, computerTurn};
+  return {isPlayerTurn, getName, isComputer, attackOpponent, recordShotFired, getShotsFired, getRandCoords, computerTurn};
 }
 
 export default Player;
