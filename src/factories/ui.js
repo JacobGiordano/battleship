e510f032b15d7efd8572599ab0cedf34d4d78860
JacobGiordano <jacob.giordano12@gmail.com>
2021-11-11 06:52:21 -0800

    Move DOM related functions to new ui module

---
+++ src/factories/ui.js
@@ -0,0 +1,27 @@
+const ui = {
+  showCurrentPlayer: currentPlayer => {
+    document.getElementById("current-player").textContent = currentPlayer;
+  },
+
+  getClickedIndex: (e) => {
+    return Array.from(e.target.parentNode.children).indexOf(e.target);
+  },
+
+  getClickedRow: clickedIndex => {
+    return Math.floor(clickedIndex / 10);
+  },
+
+  getClickedColumn: clickedIndex => {
+    return clickedIndex - (Math.floor(clickedIndex / 10) * 10);
+  },
+
+  addHitClass: squareEl => {
+    squareEl.classList.add("hit");
+  },
+
+  addMissClass: squareEl => {
+    squareEl.classList.add("miss");
+  }
+}
+
+export default ui;
