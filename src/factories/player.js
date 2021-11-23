f37c85c89bab5b9619fdef26d5b4b016547d3531
JacobGiordano <jacob.giordano12@gmail.com>
2021-11-23 09:22:01 -0800

    Comment out player logs

--- src/factories/player.js
+++ src/factories/player.js
@@ -33,12 +33,12 @@ const Player = (name, isComputerBool) => {
     } else {
       coords = ai.getRandCoords();
     }
-    console.log(coords);
+    // console.log(coords);
     if (shotsFired.indexOf(coords) > -1) {
-      console.log(`WAIT! shotsFired.indexOf(${coords}) > -1. trying again`);
+      // console.log(`WAIT! shotsFired.indexOf(${coords}) > -1. trying again`);
       computerTurn();
     } else {
-      console.log(`All clear! Firing on ${coords}!`);
+      // console.log(`All clear! Firing on ${coords}!`);
       setTimeout(() => {
         recordShotFired(coords);
         const squareIndex = ai.getIndex(coords);
