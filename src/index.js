c9ffd01b521947b2d49a3ba7c79bde20aaaf4f67
JacobGiordano <jacob.giordano12@gmail.com>
2021-11-24 06:33:16 -0800

    Remove init and Game import from index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 import "./css/styles.scss";
-import Game from "./factories/game";
 import "./factories/draggable";
-
-const initApp = () => {
-  const game = Game();
-  game.init();
-};
-
-document.addEventListener("DOMContentLoaded", initApp);
