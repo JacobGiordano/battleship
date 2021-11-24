a5c6937768b1cec22dc3a6fd34b7adbc8811d9e6
JacobGiordano <jacob.giordano12@gmail.com>
2021-11-24 12:31:37 -0800

    Change "player 1" to "you" for turn ui

--- src/factories/game.js
+++ src/factories/game.js
@@ -4,10 +4,11 @@ import ui from "../modules/ui";
 import ai from "../modules/ai";
 
 const Game = () => {
-  const player = Player("Player 1", false);
+  const player = Player("You", false);
   const computer = Player("Computer", true);
   const playerGameboard = Gameboard(player);
   const computerGameboard = Gameboard(computer);
+  let allPlayerShipsPlaced = false;
   ui.showCurrentPlayer(player.getName());
   computerGameboard.prepopulateShips(computerGameboard, ai.createRandShipsArray());
   playerGameboard.addSquareEventListeners(document.getElementById("player-1-board"));
