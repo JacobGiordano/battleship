34815ae9ec52f7885019c12a8bd8cd9222a1a728
JacobGiordano <jacob.giordano12@gmail.com>
2021-11-24 06:33:51 -0800

    Remove init from Game and return a game object

--- src/factories/game.js
+++ src/factories/game.js
@@ -4,19 +4,19 @@ import ui from "../modules/ui";
 import ai from "../modules/ai";
 
 const Game = () => {
-  const init = () => {
-    const player = Player("Player 1", false);
-    const computer = Player("Computer", true);
-    const playerGameboard = Gameboard(player);
-    const computerGameboard = Gameboard(computer);
-    ui.showCurrentPlayer(player.getName());
-    playerGameboard.prepopulateShips(playerGameboard, ai.createRandShipsArray());
-    computerGameboard.prepopulateShips(computerGameboard, ai.createRandShipsArray());
-    playerGameboard.addSquareEventListeners(document.getElementById("player-1-board"));
-    computerGameboard.addSquareEventListeners(document.getElementById("computer-board"));
-  }
+  const player = Player("Player 1", false);
+  const computer = Player("Computer", true);
+  const playerGameboard = Gameboard(player);
+  const computerGameboard = Gameboard(computer);
+  ui.showCurrentPlayer(player.getName());
+  playerGameboard.prepopulateShips(playerGameboard, ai.createRandShipsArray());
+  computerGameboard.prepopulateShips(computerGameboard, ai.createRandShipsArray());
+  playerGameboard.addSquareEventListeners(document.getElementById("player-1-board"));
+  computerGameboard.addSquareEventListeners(document.getElementById("computer-board"));
 
-  return {init};
+  return {player, computer, playerGameboard, computerGameboard};
 }
 
-export default Game;
+const game = Game();
+
+export {Game as default, game};
