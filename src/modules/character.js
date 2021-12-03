f604c7b63f147d4ce4275c9738544eab3e9ce30a
JacobGiordano <jacob.giordano12@gmail.com>
2021-12-03 06:56:05 -0800

    Add responses & screen typing to character module

--- src/modules/character.js
+++ src/modules/character.js
@@ -1,18 +1,48 @@
-const character = {
-  sunkEnemyShip: () => {
+import ai from "../modules/ai";
 
+const character = {
+  sunkEnemyShip: (shipName) => {
+    const responseArray = [
+      `Direct hit! The enemy ${shipName} has been sunk!`,
+      `Hit confirmed! That was the enemy's ${shipName}!`,
+      `Admiral, I'm receiving reports that was the enemy ${shipName}!`,
+      `That was a hit! The enemy's ${shipName} is going down, Admiral!`,
+      `We got it! The enemy's ${shipName} has been destroyed!`
+    ];
+    
+    return responseArray[ai.getRandInclusive(0, responseArray.length - 1)];
+    
   },
 
-  reportSunkenShip: () => {
-
+  reportSunkenShip: (shipName) => {
+    const responseArray = [
+      `Admiral, they got our ${shipName}!`,
+      `It can't be! That was our ${shipName}!`,
+      `I'm receiving reports we just lost our ${shipName}, Admiral!`,
+      `How?! That was the fastest ${shipName} in our fleet!`,
+      `Our ${shipName}!!!`
+    ];
+    
+    return responseArray[ai.getRandInclusive(0, responseArray.length - 1)];
+    
   },
 
   introScript: () => {
 
   },
 
-  typeTextOnScreen: () => {
-    
+  comsMsg: (string) => {
+    const comsText = document.getElementById("coms-text");
+    comsText.textContent = "";
+    let i = 0;
+    let timer = setInterval(() => {
+      if (i < string.length) {
+        comsText.textContent += string.charAt(i);
+        i++
+      } else {
+        clearInterval(timer);
+      }
+    }, 50);
   }
 }
 
